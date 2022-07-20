9f9d289e2c7e643fdd50bb28226753b6699dad91
M Wildan F <wildanfd11@gmail.com>
2022-07-20 16:35:27 +0700

    Menambahkan method untuk mendapatkan response yang berbeda beda pada tiap methodnya

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -4,13 +4,31 @@ const http = require('http');
 // Custom callback - Untuk menangani dan menanggapi sebuah request
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
-
     response.statusCode = 200;
-    response.end('<h1>Halo HTTP Server!</h1>');
+
+    // MEnggunakan Destructuring Object untuk mendapatkan nilai method (GET, POST, PUT, DELETE)
+    const { method } = request;
+
+    if(method === 'GET') {
+        response.end('<h1>Halo HTTP Server!</h1>');
+    }
+
+    if(method === 'POST') {
+        response.end('<h1>Hola</h1>');
+    }
+
+    if(method === 'PUT') {
+        response.end('<h1>AnnyeongHaseyo!</h1>');
+    }
+
+    if(method === 'DELETE') {
+        response.end('<h1>Bonjour!</h1>');
+    }
+
 };
 
 // Membuat HTTP server.
-// Menerima satu buah parameter custom callback yang digunakan sebagai request listener.
+// http.createServer() Menerima satu buah parameter custom callback yang digunakan sebagai request listener.
     // Di dalamm request listener ini berisi logika yang kita tuliskan untuk menangani dan menanggapi sebuah request
 const server = http.createServer(requestListener);
 
