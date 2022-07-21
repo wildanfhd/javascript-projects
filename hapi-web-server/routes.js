20ff4a9e60921145e8aed8d712d305b8ee44eb34
M Wildan F <wildanfd11@gmail.com>
2022-07-21 20:17:20 +0700

    Menambahkan route tambahan dengan optional path parameter dan menambahkan dokumentasinya

--- hapi-web-server/routes.js
+++ hapi-web-server/routes.js
@@ -30,6 +30,17 @@ const routes = [
         }
     },
     {
+        method: 'GET',
+        path: '/hello/{name?}', // Optional Path parameter {name?} : Jika client tidak menetapkan path parameter maka name akan bernilai stranger (default value)
+        handler: (request, h) => {
+            // {name?} merupakan parameter untuk path yang diisi oleh Client
+            // parameter path tersebut nanti akan disimpan sebagai properti pada request.params.
+            // untuk mengakses propertinya, kita gunakan object destructuring.
+            const { name = "stranger" } = request.params;
+            return `Hello, ${name}`;
+        }
+    },
+    {
         method: '*',
         path: '/{any*}',
         handler: (request, h) => {
