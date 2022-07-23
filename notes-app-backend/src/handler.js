21fbfe6c4143c4b52ec4d7c713795225c7481b13
M Wildan F <wildanfd11@gmail.com>
2022-07-23 21:16:30 +0700

    Mengimport array notes agar newNote(berisi properti objek notes)dapat dimasukkan ke dalam array notes dan Menambahkan dokumentasi

--- notes-app-backend/src/handler.js
+++ notes-app-backend/src/handler.js
@@ -1,4 +1,5 @@
 const { nanoid } = require('nanoid');
+const notes = require('./notes');
 
 const addNoteHandler = (request, h) => {
     // Mendapatkan body request menggunakan request.payload.
@@ -7,6 +8,38 @@ const addNoteHandler = (request, h) => {
     const id = nanoid(16);
     const createdAt = new Date().toISOString();
     const updatedAt = createdAt;
+
+    // newNote menampung seluruh properti objek notes
+    const newNote = {
+        title, tags, body, id, createdAt, updatedAt,
+    };
+
+    // menambahkan newNote (yang berisi properti objek notes) ke dalam array notes
+    notes.push(newNote);
+
+    const isSuccess = notes.filter(note => note.id === id).length > 0;
+
+    // Pengkondisian untuk menentukan response yang diberikan oleh server.
+        // Jika isSuccess bernilai true maka respons berhasil akan triggered, jika false sebaliknya 
+    if(isSuccess) {
+        const response = h.response({
+            status: 'success',
+            message: 'Catatan berhasil ditambahkan',
+            data: {
+                noteId: id,
+            },
+        });
+        response.code(201);
+        return response;
+    }
+    
+    // Response jika isSucces bernilai false
+    const response = h.response({
+        status: 'fail',
+        message: 'Catatan tidak ditemukan',
+    });
+    response.code(500);
+    return response
 };
 
 module.exports = { addNoteHandler };
