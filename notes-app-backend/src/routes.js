94f67d84be33085195684514e997608f7431bd20
M Wildan F <wildanfd11@gmail.com>
2022-07-26 23:28:47 +0700

    Menambahkan route untuk path GET agar notes dapat diambil

--- notes-app-backend/src/routes.js
+++ notes-app-backend/src/routes.js
@@ -1,10 +1,15 @@
-const { addNoteHandler } = require("./handler");
+const { addNoteHandler, getAllNotesHandler } = require("./handler");
 
 const routes = [
     {
         method: 'POST',
         path: '/notes',
         handler: addNoteHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: getAllNotesHandler,
     },
 ];
 
