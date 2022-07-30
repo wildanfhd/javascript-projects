66d6960a205b49dc8b97f7add2513880c694585b
M Wildan F <wildanfd11@gmail.com>
2022-07-30 23:23:49 +0700

    Menambahkan route untuk mengedit note, mengubah penulisan single quote menjadi double quote sesuai rules eslit

--- notes-app-backend/src/routes.js
+++ notes-app-backend/src/routes.js
@@ -1,21 +1,28 @@
-const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require("./handler");
+const {
+    addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler,
+} = require("./handler");
 
 const routes = [
     {
-        method: 'POST',
-        path: '/notes',
+        method: "POST",
+        path: "/notes",
         handler: addNoteHandler,
     },
     {
-        method: 'GET',
-        path: '/notes',
+        method: "GET",
+        path: "/notes",
         handler: getAllNotesHandler,
     },
     {
-        method: 'GET',
-        path: '/notes/{id}',
+        method: "GET",
+        path: "/notes/{id}",
         handler: getNoteByIdHandler,
-    }
+    },
+    {
+        method: "PUT",
+        path: "/notes/{id}",
+        handler: editNoteByIdHandler,
+    },
 ];
 
 module.exports = routes;
